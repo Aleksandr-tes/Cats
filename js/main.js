ea027db14761cad70aecb93a2d94f4f6bc9d6c01
Aleksandr-tes <91590581+Aleksandr-tes@users.noreply.github.com>
2021-11-01 16:05:53 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,9 +7,47 @@ $('.prices__card-image').slick({
   adaptiveHeight: true
 });
 
+// $('.gallery__block').slick({
+//   infinite: true,
+//   slidesToShow: 3,
+//   slidesToScroll: 1
+// });
 
 
 
+$('.gallery__block').slick({
+  infinite: true,
+  // adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  speed: 700,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
 
 
